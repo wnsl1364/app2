ddef3075b1a3edd5ffe9c3a46b8d3a21325c9a12
jooyeon Han <wnsl1364@naver.com>
2025-08-12 11:39:41 +0900

    first commit

---
+++ src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+	
+	@GetMapping
+	@ResponseBody
+	public String main() {
+		return "app2 main";
+	}
+
+}
